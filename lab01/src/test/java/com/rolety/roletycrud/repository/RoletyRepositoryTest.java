94083f0c7246ea2d3b1023b6666da7cfce611c09
Avarice111 <galyn43@gmail.com>
2018-03-18 21:00:28 +0100

    uzupelnienie testow

--- lab01/src/test/java/com/rolety/roletycrud/repository/RoletyRepositoryTest.java
+++ lab01/src/test/java/com/rolety/roletycrud/repository/RoletyRepositoryTest.java
@@ -12,12 +12,19 @@ public class RoletyRepositoryTest
 
 	@Test
     public void getAll(){
-    assertNotNull(roletyRepository.getAll());
+        //to jakieś wartości
+        Rolety rolety = new Rolety();
+        rolety.setId(3);
+        rolety.setName("Rolety najlepsze na swiecie");
+        rolety.setSize(22);
+        rolety.setPrice(12);
+        //
+        assertNotNull(roletyRepository.getAll());
     }
 
     @Test
     public void initDatabase() {
-    assertNotNull(RoletyRepositoryFactory.getInstance());
+        assertNotNull(RoletyRepositoryFactory.getInstance());
     }
 
     @Test
@@ -41,7 +48,11 @@ public class RoletyRepositoryTest
     public void deleteRolety() {
         Rolety rolety = RoletyRepositoryFactory.getInstance().getById(1);
         roletyRepository.deleteRolety(rolety);
-        assertEquals(null, RoletyRepositoryFactory.getInstance().getById(1));
+        assertNull(RoletyRepositoryFactory.getInstance().getById(1));
+        //czy inne rekordy nie zostały ruszone
+        assertFalse(RoletyRepositoryFactory.getInstance().getAll().isEmpty());
+
+        
     }
 
     @Test
@@ -50,5 +61,10 @@ public class RoletyRepositoryTest
         rolety.setName("mango_super_opcja");
         roletyRepository.updateRolety(2, rolety);
         assertEquals("mango_super_opcja", RoletyRepositoryFactory.getInstance().getById(2).getName());
+        //to samo
+        assertNotEquals("mango_super_opcja", RoletyRepositoryFactory.getInstance().getById(1).getName());
+        assertNotEquals("mango_super_opcja", RoletyRepositoryFactory.getInstance().getById(3).getName());
+        //sprawdzamy czy inne recordy nie zostały naruszone
+
     }
 }
