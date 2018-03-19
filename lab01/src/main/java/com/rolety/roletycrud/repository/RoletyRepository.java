46a5fdc64fdc3d82a60335fb7e57312336a173a3
Avarice111 <galyn43@gmail.com>
2018-03-19 15:59:41 +0100

    czesc SQL

--- lab01/src/main/java/com/rolety/roletycrud/repository/RoletyRepository.java
+++ lab01/src/main/java/com/rolety/roletycrud/repository/RoletyRepository.java
@@ -1,9 +1,16 @@
 package com.rolety.roletycrud.repository;
 
 import com.rolety.roletycrud.domain.Rolety;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface RoletyRepository {
+
+    public Connection getConnection();
+
+	public void setConnection(Connection connection) throws SQLException;
     public List<Rolety> getAll();
     public void initDatabase();
     public Rolety getById(int id);
