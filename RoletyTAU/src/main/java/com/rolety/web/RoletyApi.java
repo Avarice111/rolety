d4ce77f83670a84bcb8c365208f42207a107a148
Avarice111 <galyn43@gmail.com>
2018-04-23 13:49:59 +0200

    fixes

--- RoletyTAU/src/main/java/com/rolety/web/RoletyApi.java
+++ RoletyTAU/src/main/java/com/rolety/web/RoletyApi.java
@@ -4,16 +4,12 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.faces.convert.IntegerConverter;
-import javax.ws.rs.Path;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
--- RoletyTAU/src/test/java/com/rolety/web/RoletyRepositoryMockTest.java
+++ RoletyTAU/src/test/java/com/rolety/web/RoletyRepositoryMockTest.java
@@ -1,6 +1,7 @@
 package com.rolety.web;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -46,22 +47,22 @@ public class RoletyRepositoryMockTest {
     public void setupDatabase() throws SQLException {
         when(connectionMock.prepareStatement("INSERT INTO Rolety (Name, Price, Size) VALUES (?, ?, ?)")).thenReturn(addMock);
         when(connectionMock.prepareStatement("SELECT Id, Name, Price, Size FROM Rolety")).thenReturn(getAllMock);
-        when(connectionMock.prepareStatement("SELECT * FROM Rolety WHERE Id = ? ")).thenReturn(getByIdMock);
+        when(connectionMock.prepareStatement("DELETE FROM Rolety WHERE Id = ?")).thenReturn(deleteMock);
         when(connectionMock.prepareStatement("UPDATE Rolety SET Name = ?, Price = ?, Size = ? WHERE Id = ?")).thenReturn(updateMock);
-        when(connectionMock.prepareStatement("DELETE FROM Rolety WHERE id = ?")).thenReturn(deleteMock);
+        when(connectionMock.prepareStatement("SELECT * FROM Rolety WHERE Id = ?")).thenReturn(getByIdMock);
         roletyRepository = new RoletyRepositoryFactory();
         roletyRepository.setConnection(connectionMock);
 
         verify(connectionMock).prepareStatement("INSERT INTO Rolety (Name, Price, Size) VALUES (?, ?, ?)");
         verify(connectionMock).prepareStatement("SELECT Id, Name, Price, Size FROM Rolety");
-        verify(connectionMock).prepareStatement("SELECT * FROM Rolety WHERE Id = ? ");
+        verify(connectionMock).prepareStatement("DELETE FROM Rolety WHERE Id = ?");
         verify(connectionMock).prepareStatement("UPDATE Rolety SET Name = ?, Price = ?, Size = ? WHERE Id = ?");
-        verify(connectionMock).prepareStatement("DELETE * FROM Rolety WHERE Id = ?");
+        verify(connectionMock).prepareStatement("SELECT * FROM Rolety WHERE Id = ?");
 
     }
 
     @Test
-    public void checkAdding() throws Exception {
+    public void checkAdding() throws SQLException {
         when(addMock.executeUpdate()).thenReturn(1);
         Rolety wyborne = new Rolety();
 
@@ -129,7 +130,7 @@ public class RoletyRepositoryMockTest {
     public void checkGetById() throws SQLException {
         AbstractResultSet mockedResutSet = mock(AbstractResultSet.class);
         when(mockedResutSet.next()).thenCallRealMethod();
-        when(mockedResutSet.getInt("id")).thenCallRealMethod();
+        when(mockedResutSet.getInt("Id")).thenCallRealMethod();
         when(mockedResutSet.getString("Name")).thenCallRealMethod();
         when(mockedResutSet.getInt("Price")).thenCallRealMethod();
         when(mockedResutSet.getInt("Size")).thenCallRealMethod();
@@ -138,7 +139,7 @@ public class RoletyRepositoryMockTest {
         assertEquals(1, roletyRepository.getById(1).getId());
 
         verify(getByIdMock, times(1)).executeQuery();
-        verify(mockedResutSet, times(1)).getInt("id");
+        verify(mockedResutSet, times(1)).getInt("Id");
         verify(mockedResutSet, times(1)).getString("Name");
         verify(mockedResutSet, times(1)).getInt("Price");
         verify(mockedResutSet, times(1)).getInt("Size");
@@ -149,16 +150,16 @@ public class RoletyRepositoryMockTest {
     public void getAllTest() throws SQLException {
         AbstractResultSet mockedResultSet = mock(AbstractResultSet.class);
         when(mockedResultSet.next()).thenCallRealMethod();
-        when(mockedResultSet.getInt("id")).thenCallRealMethod();
+        when(mockedResultSet.getInt("Id")).thenCallRealMethod();
         when(mockedResultSet.getString("Name")).thenCallRealMethod();
         when(mockedResultSet.getInt("Price")).thenCallRealMethod();
         when(mockedResultSet.getInt("Size")).thenCallRealMethod();
         when(getAllMock.executeQuery()).thenReturn(mockedResultSet);
 
-        assertEquals(1, roletyRepository.getAll());
+        assertEquals(1, roletyRepository.getAll().size());
 
         verify(getAllMock, times(1)).executeQuery();
-        verify(mockedResultSet, times(1)).getInt("id");
+        verify(mockedResultSet, times(1)).getInt("Id");
         verify(mockedResultSet, times(1)).getString("Name");
         verify(mockedResultSet, times(1)).getInt("Price");
         verify(mockedResultSet, times(1)).getInt("Size");
