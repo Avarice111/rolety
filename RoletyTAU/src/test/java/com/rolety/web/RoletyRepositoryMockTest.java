c8a694f0953884a7e062d707e250f12d246de668
Avarice111 <galyn43@gmail.com>
2018-04-23 14:04:02 +0200

    fixes

--- RoletyTAU/src/test/java/com/rolety/web/RoletyRepositoryMockTest.java
+++ RoletyTAU/src/test/java/com/rolety/web/RoletyRepositoryMockTest.java
@@ -1,7 +1,6 @@
 package com.rolety.web;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
